258dbe1e3d4277f797dbc24b0cf4484c63453a7e
HRPassionGuy <mgrmarcus@hrpassion.com>
2026-03-24 22:43:55 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['lh3.googleusercontent.com'], // For profile images if using Google auth
+  experimental: {
+    serverComponentsExternalPackages: ['@react-pdf/renderer'],
   },
 }
 
